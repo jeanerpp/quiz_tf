0841aefdaef1605be3f426aa5eabd0867cdfff36
xujian <jian.sh.xu@outlook.com>
2026-04-17 13:22:32 +0800

    refactor networking module

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -1,37 +1,50 @@
-resource "aws_subnet" "public-subnet" {
+# Create public subnet for instances like NAT Gateway
+resource "aws_subnet" "public_subnet" {
   vpc_id = var.vpc_id
+
   cidr_block = var.az.public_subnet_cidr
   availability_zone = var.az.availability_zone
   map_public_ip_on_launch = false  # only NAT Gateway will be in public subnet, so disable public IP assignment
+
   tags = var.tags
 }
 
-resource "aws_subnet" "control-subnet" {
+# Create control subnet for control plane instances like application servers
+resource "aws_subnet" "control_subnet" {
     vpc_id = var.vpc_id
+
     cidr_block = var.az.control_subnet_cidr
     availability_zone = var.az.availability_zone
+
     tags = var.tags
 }
 
-resource "aws_subnet" "data-subnet" {
+# Create data subnet for data plane instances like databases
+resource "aws_subnet" "data_subnet" {
     vpc_id = var.vpc_id
+
     cidr_block = var.az.data_subnet_cidr
     availability_zone = var.az.availability_zone
+
     tags = var.tags
 }
-    
-resource "aws_eip" "eip" {
+
+# Elastic public IP for NAT gateway
+resource "aws_eip" "nat_eip" {
     domain = "vpc"
     tags = var.tags
 }
 
-resource "aws_nat_gateway" "nat-gateway" {
-    allocation_id = aws_eip.eip.id
-    subnet_id = aws_subnet.public-subnet.id
+# NAT Gateway for control/data networks internet access
+resource "aws_nat_gateway" "nat_gateway" {
+    allocation_id = aws_eip.nat_eip.id
+    subnet_id = aws_subnet.public_subnet.id
+
     tags = var.tags
 }
 
-resource "aws_route_table" "public-route-table" {
+# Routing table to IGW
+resource "aws_route_table" "public_route_table" {
   vpc_id = var.vpc_id
 
   route {
@@ -42,28 +55,32 @@ resource "aws_route_table" "public-route-table" {
   tags = var.tags
 }
 
-resource "aws_route_table" "private-route-table" {
+# Routing table to NAT Gateway
+resource "aws_route_table" "private_route_table" {
   vpc_id = var.vpc_id
 
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat-gateway.id
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id
   }
 
   tags = var.tags
 }
 
-resource "aws_route_table_association" "public-subnet-assoc" {
-  subnet_id      = aws_subnet.public-subnet.id
-  route_table_id = aws_route_table.public-route-table.id
+# Public subnet use IGW for internet
+resource "aws_route_table_association" "public_subnet_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
 }
 
-resource "aws_route_table_association" "control-subnet-assoc" {
-  subnet_id      = aws_subnet.control-subnet.id
-  route_table_id = aws_route_table.private-route-table.id
+# Control subnet uses NAT gateway for internet
+resource "aws_route_table_association" "control_subnet_assoc" {
+  subnet_id      = aws_subnet.control_subnet.id
+  route_table_id = aws_route_table.private_route_table.id
 }
 
-resource "aws_route_table_association" "data-subnet-assoc" {
-  subnet_id      = aws_subnet.data-subnet.id
-  route_table_id = aws_route_table.private-route-table.id
+# Data subnet uses NAT gateway for internet
+resource "aws_route_table_association" "data_subnet_assoc" {
+  subnet_id      = aws_subnet.data_subnet.id
+  route_table_id = aws_route_table.private_route_table.id
 }
--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -1,11 +1,11 @@
 output "public_subnet_id" {
-  value = aws_subnet.public-subnet.id
+  value = aws_subnet.public_subnet.id
 }
 
 output "control_subnet_id" {
-  value = aws_subnet.control-subnet.id
+  value = aws_subnet.control_subnet.id
 }
 
 output "data_subnet_id" {
-  value = aws_subnet.data-subnet.id
+  value = aws_subnet.data_subnet.id
 }
